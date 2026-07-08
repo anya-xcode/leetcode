a5a760eed6381ac275389a55aa98156f0b51f03f
Ananya Gupta <ananynagupta@gmail.com>
2026-07-08 17:18:25 +0530

    Time: 192 ms (50.06%), Space: 34.7 MB (78.04%) - LeetHub

--- 0239-sliding-window-maximum/0239-sliding-window-maximum.py
+++ 0239-sliding-window-maximum/0239-sliding-window-maximum.py
@@ -4,12 +4,12 @@ class Solution:
         dq = deque()
         ans = []
         for i in range(len(nums)):
-            while dq and dq[0]<=i-k: # taaki window same rhe 
+            while dq and dq[0]<=i-k: # taaki currect window ke bahar ke indices remove krr rhe hai  
                 dq.popleft()
-            while dq and nums[dq[-1]]<nums[i]:
+            while dq and nums[dq[-1]]<nums[i]: #remove smaller elements from back 
                 dq.pop()
             dq.append(i)    
-            if i>=k-1:
+            if i>=k-1: # ye ki 2 length ki window n aaye 
                 ans.append(nums[dq[0]])
         
         return ans        
