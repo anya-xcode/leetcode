d1f2ed9daa86de15ca898491a41189438e9dd560
Ananya Gupta <ananynagupta@gmail.com>
2026-07-09 13:43:06 +0530

    Time: 1249 ms (6.65%), Space: 21 MB (58.01%) - LeetHub

--- 0503-next-greater-element-ii/0503-next-greater-element-ii.py
+++ 0503-next-greater-element-ii/0503-next-greater-element-ii.py
@@ -1,19 +1,39 @@
+# class Solution:
+#     def nextGreaterElements(self, nums: List[int]) -> List[int]:
+#         n = len(nums)
+#         ans = []
+
+#         for i in range(n):
+#             greater = -1
+
+#             for j in range(1, n + 1):
+#                 if nums[(i + j) % n] > nums[i]:
+#                     greater = nums[(i + j) % n]
+#                     break
+
+#             ans.append(greater)
+
+#         return ans     
+
 class Solution:
     def nextGreaterElements(self, nums: List[int]) -> List[int]:
-        n = len(nums)
         ans = []
-
-        for i in range(n):
-            greater = -1
-
-            for j in range(1, n + 1):
-                if nums[(i + j) % n] > nums[i]:
-                    greater = nums[(i + j) % n]
+        for i in range(len(nums)):
+            greater = float("-inf")
+            for j in range(i+1,len(nums)):
+                if nums[j]>nums[i]:
+                    greater = nums[j]
                     break
-
-            ans.append(greater)
-
-        return ans     
+            if greater ==float("-inf"):
+                for k in range(0,i):
+                    if nums[k]>nums[i]:
+                        greater = nums[k]
+                        break
+            ans.append(greater) 
+        for i in range(0,len(ans)):
+            if ans[i]==float('-inf'):
+                ans[i]=-1               
+        return ans                
 
 
 
